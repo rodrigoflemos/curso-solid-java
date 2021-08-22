c174443b7cbd28b1519b2482b3b6993838cd5a0c
Rodrigo Lemos <rodrigoflemos@gmail.com>
2021-08-22 16:23:03 -0300

    OCP - Extracao das validacoes do reajuste para classes separadas
    - Criação de interface de validacoes de reajuste
    - Criacao de classes de validacao estendendo a interface
    - Inclusao de lista de validacoes no construtor da classe ReajusteService

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -6,17 +6,21 @@ import br.com.alura.rh.model.Funcionario;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 public class ReajusteService {
 
-    public void reajustarSalarioFuncionario(Funcionario funcionario, BigDecimal aumento) {
+    private List<ValidacaoReajuste> validacoes;
 
-        BigDecimal salarioAtual = funcionario.getSalario();
-        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
-        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-        }
-        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+    public ReajusteService(List<ValidacaoReajuste> validacoes) {
+        this.validacoes = validacoes;
+    }
+
+    public void reajustarSalarioFuncionario(Funcionario funcionario, BigDecimal aumento) {
+        this.validacoes.forEach(v -> v.validar(funcionario, aumento));
+
+        BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
         funcionario.atualizarSalario(salarioReajustado);
     }
 }
